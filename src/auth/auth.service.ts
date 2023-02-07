bf054f790f57f6e634f820171bec25cdc7bf9800
Georgio Sorin <georgio.sorin@gmail.com>
2023-02-07 08:23:47 +0100

    API V1 encrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,11 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
-    if (user && user.password === pass) {
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (!isMatch) {
+      throw new BadRequestException("password doesn't match");
+    } else {
       const { password, ...result } = user;
       return result;
     }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,16 +2,24 @@ import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserSchema } from './user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
     const result = UserSchema.validate(createUserDto);
     if (result.error) {
       const errorMessages = result.error.details.map((d) => d.message).join();
       throw new BadRequestException(errorMessages);
     }
+
+    const saltOrRounds = await bcrypt.genSalt();
+
+    createUserDto.password = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds,
+    );
     return this.userService.create(createUserDto);
   }
 }
